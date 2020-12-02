935812d76d4352a46c3267b86b690b9471fd7729
tsanona <tsanona@gmail.com>
2020-12-02 18:38:56 +0100

    errors correction

--- graph_vis.py
+++ graph_vis.py
@@ -17,7 +17,8 @@ layouts = {
     "fruchterman_reingold": nx.fruchterman_reingold_layout,
     "spiral": nx.spiral_layout,}
 
-def graph_plotter(A: np.ndarray, **draw_args)
+
+def graph_plotter(A: np.ndarray, **draw_args):
     g = nx.convert_matrix.from_numpy_matrix(A)
     nx.draw(g, node_size=50, **draw_args)
     plt.show()
@@ -34,18 +35,18 @@ if __name__ == '__main__':
                         'shell, spring, spectral, fruchterman_reingold')
     args = parser.parse_args()
 
-if args.pickled_files:
-    place = args.pickled_files
-    place_path = path.Path("../data")/place
-    with open(place_path/f"adj_mx_{place.split("_")[1]}.pkl", "rb") as f:
-        sensor_ids, sensor_id_to_ind, A = pickle.load(f, encoding='latin-1')
-    pos = np.load(place_path/f'pos_{place.split("_")[1]}.npy')
-    graph_plotter(A, pos=pos)
+    if args.pickled_files:
+        place = args.pickled_files
+        place_path = path.Path("../data")/place
+        with open(place_path/f"adj_mx_{place.split('_')[1]}.pkl", "rb") as f:
+            sensor_ids, sensor_id_to_ind, A = pickle.load(f, encoding='latin-1')
+        pos = np.load(place_path/f'pos_{place.split("_")[1]}.npy')
+        graph_plotter(A, pos=pos)
 
-elif args.seattle_data:
-    A = np.load("Seattle_Loop_Dataset/Loop_Seattle_2015_A.npy")
-    mp = pd.read_csv("Seattle_Loop_Dataset/nodes_loop_mp_list.csv")["milepost"]
-    mp = list(map(lambda entry: entry[1:4], mp))
-    col = list(map(lambda entry: 'purple' if entry=='090' else 'y' if entry=='405' else 'b' if entry=='520' else 'r', mp))
-    graph_plotter(A, node_color=col, layout=layouts[args.layout])
+    elif args.seattle_data:
+        A = np.load("Seattle_Loop_Dataset/Loop_Seattle_2015_A.npy")
+        mp = pd.read_csv("Seattle_Loop_Dataset/nodes_loop_mp_list.csv")["milepost"]
+        mp = list(map(lambda entry: entry[1:4], mp))
+        col = list(map(lambda entry: 'purple' if entry=='090' else 'y' if entry=='405' else 'b' if entry=='520' else 'r', mp))
+        graph_plotter(A, node_color=col, layout=layouts[args.layout])
 
