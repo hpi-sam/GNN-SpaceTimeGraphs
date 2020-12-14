4a5159c11d9033506e95f0d8619d49c843e23e14
tsanona <tsanona@gmail.com>
2020-12-14 23:04:30 +0100

    dataset

---
+++ gnn/dataset.py
@@ -0,0 +1,14 @@
+from gnn.utils import load_data
+from torch.utils.data import Dataset
+import torch
+
+
+class TraficDataset(Dataset):
+    def __init__(self, args):
+        _, self.features_train, self.labels_train = load_data(args.train_file)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        return {"features": self.features_train[idx, :, :], "train": self.labels_train[idx, :, :]}
+
